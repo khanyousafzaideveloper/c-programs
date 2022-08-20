a603ad8bc14a4a2218db6d3629be1789ed24f7c7
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-08-20 20:00:59 +0500

    ..

--- practicefork.c
+++ practicefork.c
@@ -3,8 +3,9 @@
 
 void main()
 {
-    pid_t id;
-    id = fork();
-    printf("\nThis is some random code : %d", id);
+    pid_t id1, id2;
+    id1 = fork();
+    id2 = fork();
+    printf("\nThis is some random code : %d and %d", id1, id2);
 
 }
