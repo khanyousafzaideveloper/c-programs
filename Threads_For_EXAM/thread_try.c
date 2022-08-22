b7e76e42ea8d27b1b6a715dc4bbfd894ec3da9de
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-08-22 23:07:26 +0500

    ,,,

--- Threads_For_EXAM/thread_try.c
+++ Threads_For_EXAM/thread_try.c
@@ -6,7 +6,6 @@ void *MyFunction(void *x)
 {
     printf("Inside a Function\n");
 }
-
 int main()
 {
     pthread_t thread_id;
@@ -14,5 +13,6 @@ int main()
     printf("Outside a Function\n");
 
     pthread_create(&thread_id, NULL, MyFunction, NULL);
+    
     pthread_join(thread_id, NULL);
 }
