370bc633bf41e6d6c683b57065572fe8e5705cf2
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-08-23 09:31:36 +0500

    threads_

---
+++ Threads_For_EXAM/threads_all_arg.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void *MyFunction(void *arg)
+{
+    printf("I am string inside a Function\n");
+    pthread_exit(0);
+}
+
+int main()
+{
+    pthread_t thread_id;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+     int num = 123;
+    printf("Hi, This is some random String outside function\n");
+    pthread_create(&thread_id, &attr, MyFunction, &num);
+
+    pthread_join(thread_id, NULL);
+
+}
