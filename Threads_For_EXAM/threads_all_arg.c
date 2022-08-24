b41ad56ae5bba9a183c931f7d364c85de968562f
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-08-24 12:18:31 +0500

    some practice

--- Threads_For_EXAM/threads_all_arg.c
+++ Threads_For_EXAM/threads_all_arg.c
@@ -11,11 +11,11 @@ void *MyFunction(void *arg)
 int main()
 {
     pthread_t thread_id;
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-     int num = 123;
+    //pthread_attr_t attr;
+    //pthread_attr_init(&attr);
+    // int num = 123;
     printf("Hi, This is some random String outside function\n");
-    pthread_create(&thread_id, &attr, MyFunction, &num);
+    pthread_create(&thread_id, NULL, MyFunction, NULL);
 
     pthread_join(thread_id, NULL);
 
