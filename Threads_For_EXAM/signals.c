55585ee5d8c80fcc6a442d9c933b559916f55140
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-08-27 11:25:39 +0500

    signals

---
+++ Threads_For_EXAM/signals.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void interrupt_handler(int num)
+{
+    printf("I won't die\n");
+}
+
+int main()
+{
+    signal(SIGINT, interrupt_handler);
+    while (1)
+    {
+        printf("Wasting your time\n");
+        sleep(1);
+    }
+    return 0;
+}
