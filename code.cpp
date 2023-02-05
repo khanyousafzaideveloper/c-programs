1ded5c9c84a1035be439aa923a348bf3025e2adb
Ahmad Khan Yousafzai <khanyousafzaideveloper@gmail.com>
2023-02-05 08:37:58 +0500

    q1 solution

--- code.cpp
+++ code.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int last=0, next = 0, sum;
-    do{
-        sum = last + next;
-        last = sum;
-       
-        cout << "Enter a number you want to sum: ";
-        cin >> next; 
+    for(int a=0; a>=4; a++)
+    {
+        cout << "Hello world" << endl;
     }
-    while(next!=0);
-     cout << "sum: " << endl;
-     cout << sum << endl;
-    return 0;
 }
